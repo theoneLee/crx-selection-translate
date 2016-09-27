373fcc59954eb915e929dced3857fe02444e81ea
lmk123 <me@limingkai.cn>
2016-09-27 16:16:15 +0800

    修复拖动的问题

--- src/content-script/app/app.vue
+++ src/content-script/app/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="__st-container__">
     <div class="__translate-btn__" :style="btnStyle">译</div>
-    <div class="__win__" :style="winStyle" v-el:win>
+    <div class="__win__" v-el:win>
       <div class="__win-header__"><span>固定</span><span @click="hideWin">关闭</span></div>
       <div>body</div>
     </div>
@@ -42,11 +42,22 @@
 
       winStyle () {
         const { win } = this
-
         return {
           display: win.show ? 'block' : 'none',
           top: win.top + 'px',
           left: win.left + 'px'
+        }
+      }
+    },
+
+    watch: {
+      // 手动更改 win 的样式，因为遇到相同的样式时 Vue 不会更新 dom，
+      // 但 win 的样式可能会因为用户拖动而被 Draggabilly 改变，
+      // 所以就会导致下次显示翻译窗口时的位置是上次用户拖动的位置
+      winStyle: {
+        deep: true,
+        handler (style) {
+          Object.assign(this.$els.win.style, style)
         }
       }
     },
@@ -76,6 +87,10 @@
 
     .__translate-btn__ {
       position: absolute;
+    }
+
+    .__win__ {
+      display: none;
     }
   }
 </style>
